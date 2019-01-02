8bb1e2a35691248da598c47a25e8fec8870fa710
Naren Soni <soninaren@users.noreply.github.com>
2019-01-02 13:37:13 -0800

    Adding json validation build (#51)

---
+++ build.ps1
@@ -0,0 +1,34 @@
+$ErrorActionPreference = "Stop"
+
+function LogOperationStart($message) {
+    Write-Host $message -NoNewline
+}
+
+function LogSuccess() {
+    Write-Host -ForegroundColor Green "...Done"
+}
+
+function LogErrorAndExit($errorMessage, $exception) {
+    Write-Host -ForegroundColor Red "...Failed" 
+    Write-Host $errorMessage -ForegroundColor Red
+    if ($exception -ne $null) {
+        Write-Host $exception -ForegroundColor Red | format-list -force
+    }    
+    Exit
+}
+
+Function validateFeedJson()
+{
+    try {
+        $file = Get-Content ".\cli-feed-v3.json" -Raw
+        $jsonObj = ConvertFrom-Json $file
+    } catch  {
+        LogErrorAndExit "The feed content is not valid JSON"  $_.Exception
+    }
+}
+
+# start tests
+
+LogOperationStart "Checking if the feed content is valid json"
+validateFeedJson
+LogSuccess 
