aaea73dc570f0c068135c8d0101e435b4080c8e8
soninaren <soninaren@users.noreply.github.com>
2019-06-02 13:20:22 -0700

    Setting up azure dev ops build pipeline

---
+++ GenerateToolingFeed/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenerateToolingFeed
+{
+    public class Constants
+    {
+        public static readonly Dictionary<string, string> OperatingSystem = new Dictionary<string, string>()
+            {
+                { "linux" ,"linux" },
+                { "macos" ,"osx" },
+                { "windows" ,"win" }
+            };
+
+        public static readonly Dictionary<string, string> Architecture = new Dictionary<string, string>()
+            {
+                { "x64" ,"x64" },
+                { "x86" ,"x86" }
+            };
+    }
+}
